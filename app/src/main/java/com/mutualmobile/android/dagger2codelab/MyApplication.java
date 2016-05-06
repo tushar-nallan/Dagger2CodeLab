7b24dd9f37f251ab317b9248c29d044ca9abce7f
Tushar Acharya <tushar.acharya@mutualmobile.com>
2016-05-06 22:52:39 +0530

    Added Application

---
+++ app/src/main/java/com/mutualmobile/android/dagger2codelab/MyApplication.java
@@ -0,0 +1,15 @@
+package com.mutualmobile.android.dagger2codelab;
+
+import android.app.Application;
+
+/**
+ * Created by tushar on 5/6/16.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // ToDo Create component here
+    }
+}
